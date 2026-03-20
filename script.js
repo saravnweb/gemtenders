ada105db2dcb848166852b5752d7ad1ca0bbf293
SARAVN <saravn.ent@gmail.com>
2026-03-20 10:47:05 +0530

    chore: remove debug artifacts, logs, and temp files from repo

--- script.js
+++
@@ -1,7 +0,0 @@
-const html = require('fs').readFileSync('tmp.html', 'utf8');
-const metas = html.match(/<meta[^>]*>/g);
-if(metas) {
-  metas.forEach(m => {
-    if(m.includes('og:') || m.includes('twitter:') || m.includes('name="description"')) console.log(m);
-  });
-}
